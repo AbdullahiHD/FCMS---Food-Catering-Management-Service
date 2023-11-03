6bc3218fcd1fab44e683367c267743710d359848
fahadenough <109071591+fahadenough@users.noreply.github.com>
2023-11-03 21:39:27 +0800

    Created StaffProfilePage.php
    basic html structure to be later styled and populated from database

---
+++ FCMS-PHP/StaffProfilePage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <header>
+        <h1>User Profile</h1>
+    </header>
+    <main>
+        <section>
+            <h2>User Information</h2>
+
+            <p><strong>User ID:</strong> 12345</p>
+            <p><strong>First Name:</strong> John</p>
+            <p><strong>Last Name:</strong> Doe</p>
+            <p><strong>Email:</strong> john@example.com</p>
+            <p><strong>Phone:</strong> (123) 456-7890</p>
+            <p><strong>Department:</strong> HR</p>
+            <p><strong>Position:</strong> Manager</p>
+            <p><strong>Joining Date:</strong> 2023-01-15</p>
+        </section>
+
+        <section>
+            <h2>Additional Details</h2>
+        </section>
+    </main>
+
+    <footer>
+    </footer>
+</body>
+</html>
