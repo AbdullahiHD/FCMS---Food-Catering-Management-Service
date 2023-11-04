4c2db01d18ca1df62c9ab4cbc7b2f76c8c6031a4
fahadenough <109071591+fahadenough@users.noreply.github.com>
2023-11-04 16:10:58 +0800

    Updated staffprofilepage and created staff table in local database
    The page should now display the details of the staff logged in to the system

--- FCMS-PHP/StaffProfilePage.php
+++ FCMS-PHP/StaffProfilePage.php
@@ -1,34 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
-</head>
-<body>
-    <header>
-        <h1>User Profile</h1>
-    </header>
-    <main>
-        <section>
-            <h2>User Information</h2>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fcms";
 
-            <p><strong>User ID:</strong> 12345</p>
-            <p><strong>First Name:</strong> John</p>
-            <p><strong>Last Name:</strong> Doe</p>
-            <p><strong>Email:</strong> john@example.com</p>
-            <p><strong>Phone:</strong> (123) 456-7890</p>
-            <p><strong>Department:</strong> HR</p>
-            <p><strong>Position:</strong> Manager</p>
-            <p><strong>Joining Date:</strong> 2023-01-15</p>
-        </section>
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        <section>
-            <h2>Additional Details</h2>
-        </section>
-    </main>
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    <footer>
-    </footer>
-</body>
-</html>
+// Start a session (if not already started)
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+
+    // Query to fetch user information from the "users" table
+    $query = "SELECT * FROM users WHERE UserId = $userId";
+    $result = $conn->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        $permission = $user['Permission'];
+
+        // Check if the user is a staff member
+        if ($permission === 'staff') {
+            // Query to fetch staff-specific information from the "staff" table
+            $staffQuery = "SELECT * FROM staff WHERE UserId = $userId";
+            $staffResult = $conn->query($staffQuery);
+
+            if ($staffResult && $staffResult->num_rows > 0) {
+                $staff = $staffResult->fetch_assoc();
+
+                // Display user and staff details here
+                echo '<h1>User Profile</h1>';
+                echo '<h2>User Information</h2>';
+                echo '<p><strong>User ID:</strong> ' . $user['UserId'] . '</p>';
+                echo '<p><strong>Username:</strong> ' . $user['Username'] . '</p>';
+                echo '<p><strong>Email:</strong> ' . $user['Email'] . '</p>';
+                echo '<p><strong>Permission:</strong> ' . $user['Permission'] . '</p>';
+
+                echo '<h2>Staff Information</h2>';
+                echo '<p><strong>First Name:</strong> ' . $staff['FirstName'] . '</p>';
+                echo '<p><strong>Last Name:</strong> ' . $staff['LastName'] . '</p>';
+                echo '<p><strong>Joining Date:</strong> ' . $staff['JoiningDate'] . '</p>';
+                echo '<p><strong>Position:</strong> ' . $staff['Position'] . '</p>';
+            } else {
+                echo 'Staff information not found.';
+            }
+        } else {
+            echo 'You do not have permission to view this page.';
+        }
+    } else {
+        echo 'User not found.';
+    }
+} else {
+    echo 'User not logged in.';
+}
+
+// Close the database connection
+$conn->close();
+?>
