4d40af8d9a6b91f06bfb8ecbd6cffa3def508e0f
AbdullahiHD <144151136+AbdullahiHD@users.noreply.github.com>
2023-11-10 21:04:21 +0800

    Created RevenueStatistic.php
    Create RevenusStatistic.php

---
+++ FCMS-PHP/RevenueStatistic.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Online Food and Beverage Catering Service">
+    <meta name="keywords" content="Create Customer Account">
+    <meta name="author" content="Abdullahi">
+    <title>Revenue Statistic View</title>
+
+    <!-- Link the general layout css -->
+    <link rel="stylesheet" href="../FCMS-Assets/Main.css"> 
+
+    <!-- Link to this page's css -->
+    <link rel="stylesheet" href="../FCMS-CSS/CreateDetails.css">
+
+    <!-- Including D3 Library -->
+    <script src="https://d3js.org/d3.v6.min.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#" class="logolink">
+                <img src="../FCMS-Assets/images/culinarycue.png" width="100" height="60" alt="CulinaryCue - Home">
+            </a>
+            <ul>
+                <li><a href="../FCMS-HTML/TahaIndex.html">Home</a></li>
+                <li><a href="../FCMS-HTML/TahaIndex.html">Menu</a></li>
+                <li><a href="../FCMS-HTML/TahaIndex.html">About</a></li>
+                <li><a href="../FCMS-HTML/TahaIndex.html">Contact</a></li>
+            </ul>
+            <!-- <a href="../FCMS-PHP/Login.php" class="registrationbutton">Login</a> -->
+        </nav>
+    </header>
+    
+    <!-- Brief Heading and content -->
+    <div class="hcontent">
+        <br>
+        <h1> Revenue Statistics</h1>
+    </div>
+
+    <div id="chart-container">
+        <svg id="bar-chart"></svg>
+    </div>
+
+    <!-- Your PHP script for database connection and data fetching should be placed here. -->
+    <?php
+    // Database configuration
+    $host = "localhost"; // Corrected variable name here
+    $username = "root";
+    $password = "";
+    $database = "FCMS"; // Corrected variable name here
+
+    // Create database connection
+    $conn = new mysqli($host, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // SQL query to count customers per city
+    $sql = "SELECT City, COUNT(*) as NumberOfCustomers FROM customers GROUP BY City";
+    $result = $conn->query($sql);
+
+    $customerData = array();
+    while ($row = $result->fetch_assoc()) {
+        $customerData[] = $row;
+    }
+
+    // Free result set
+    $result->close();
+
+    // Close connection
+    $conn->close();
+
+    // Print the data in JSON format
+    echo "<script>var customerData = " . json_encode($customerData) . ";</script>";
+    ?>
+
+    <!-- Including Validation and D3 scripts -->
+    <script src="../FCMS-JavaScripts/Validation.js"></script>
+    <!-- <script src="../FCMS-JavaScripts/CustomerD3.js"></script> -->
+
+        <!-- <button class="sortAsc-button">Sort - Ascending </button>
+        <button class="sortDesc-button">Sort - Descending</button>
+        -->
+
+        <!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
+    <footer class="footer">
+    <ul class="social-icon">
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+      <li class="menu__item"><a class="menu__link" href="../FCMS-HTML/TahaIndex.html">Home</a></li>
+      <li class="menu__item"><a class="menu__link" href="../FCMS-HTML/TahaIndex.html">Menu</a></li>
+      <li class="menu__item"><a class="menu__link" href="../FCMS-HTML/TahaIndex.html">About</a></li>
+      <li class="menu__item"><a class="menu__link" href="../FCMS-HTML/TahaIndex.html">Our Team</a></li>
+      <li class="menu__item"><a class="menu__link" href="../FCMS-HTML/TahaIndex.html">Contact1</a></li>
+    </ul>
+
+  </footer>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+
+</html>
+
